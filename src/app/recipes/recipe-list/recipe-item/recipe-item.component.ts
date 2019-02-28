85ee3869f638dc9c9d655f42a16b7882e3e8d260
Brian De Maio <bdemaio1@gmail.com>
2019-02-28 13:50:23 -0500

    Attempting to display recipe information to recipe details component

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, Input } from "@angular/core";
+import { Component, OnInit, Input, Output } from "@angular/core";
 import { Recipe } from "../../recipe.model";
+import { EventEmitter } from "@angular/core";
 
 @Component({
   selector: "app-recipe-item",
@@ -8,8 +9,13 @@ import { Recipe } from "../../recipe.model";
 })
 export class RecipeItemComponent implements OnInit {
   @Input() recipe: Recipe;
+  @Output() recipeSelected = new EventEmitter<void>();
 
   constructor() {}
 
   ngOnInit() {}
+
+  onSelected() {
+    this.recipeSelected.emit();
+  }
 }
