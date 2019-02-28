1370940c669fbf2e2983beced0a45efeaf8a67a8
Brian De Maio <bdemaio1@gmail.com>
2019-02-28 11:44:31 -0500

    Switching between recipe and shopping list view, dependent on onNavigate method within app component ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'angular-recipe';
+  title: string = "angular-recipe";
+  loadedFeature: string = "recipe";
+
+  //Which navigation feature is requested
+  onNavigate(feature: string) {
+    this.loadedFeature = feature;
+  }
 }
