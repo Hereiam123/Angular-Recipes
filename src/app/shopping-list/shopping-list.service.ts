270428e39c51a14a175ea771e1e57f95e233514e
Brian De Maio <bdemaio1@gmail.com>
2019-03-11 13:59:26 -0400

    Now adding ingredients to shopping list through shopping list service

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,3 +1,18 @@
-export class ShoppingListService{
-    
+import { Ingredient } from "../shared/ingredient.model";
+import { EventEmitter } from "@angular/core";
+export class ShoppingListService {
+  ingredientsChanged = new EventEmitter<Ingredient[]>();
+  private ingredients: Ingredient[] = [
+    new Ingredient("Potato", 5),
+    new Ingredient("Tomato", 5)
+  ];
+
+  getIngredients() {
+    return this.ingredients.slice();
+  }
+
+  addIngredient(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+    this.ingredientsChanged.emit(this.ingredients.slice());
+  }
 }
