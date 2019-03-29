2096127d9be86c3909aead9d4e8e746bfa631726
hereiam123 <bdemaio1@gmail.com>
2019-03-29 19:08:43 -0400

    Fixing onDestory for recipe list component

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -51,6 +51,10 @@ export class RecipeEditComponent implements OnInit {
     );
   }
 
+  onDeleteIngredient(index: number) {
+    (<FormArray>this.recipeForm.get("ingredients")).removeAt(index);
+  }
+
   getControls() {
     return (<FormArray>this.recipeForm.get("ingredients")).controls;
   }
