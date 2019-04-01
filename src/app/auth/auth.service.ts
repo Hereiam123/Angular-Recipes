98123c782a114d1e056a9a9b576e1598983ab024
Brian De Maio <bdemaio1@gmail.com>
2019-04-01 12:03:43 -0400

    Protecting routes that shouldn't be accessed when not logged in, and rerouting to Sign up and Sign in form

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,7 +1,13 @@
 import * as firebase from "firebase";
+import { Router } from "@angular/router";
+import { Injectable } from "@angular/core";
 
+@Injectable()
 export class AuthService {
   token: string;
+
+  constructor(private router: Router) {}
+
   signUpUser(email: string, password: string) {
     firebase
       .auth()
@@ -13,18 +19,20 @@ export class AuthService {
     firebase
       .auth()
       .signInWithEmailAndPassword(email, password)
-      .then(response =>
+      .then(response => {
+        this.router.navigate(["/"]);
         firebase
           .auth()
           .currentUser.getIdToken()
           .then((token: string) => {
             this.token = token;
-          })
-      )
+          });
+      })
       .catch(error => console.log(error));
   }
 
   logout() {
+    this.router.navigate(["/signin"]);
     firebase.auth().signOut();
     this.token = null;
   }
