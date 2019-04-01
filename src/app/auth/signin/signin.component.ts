0c81e29da6b85a3e8c524c5a5b984bd41bac087d
Brian De Maio <bdemaio1@gmail.com>
2019-04-01 09:17:02 -0400

    Working on form stylings

--- src/app/auth/signin/signin.component.ts
+++ src/app/auth/signin/signin.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { NgForm } from "@angular/forms";
 
 @Component({
-  selector: 'app-signin',
-  templateUrl: './signin.component.html',
-  styleUrls: ['./signin.component.css']
+  selector: "app-signin",
+  templateUrl: "./signin.component.html",
+  styleUrls: ["./signin.component.css"]
 })
 export class SigninComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  onSignup(form: NgForm) {
+    const email = form.value.email;
+    const password = form.value.password;
   }
-
 }
