dd9f86f6cf497a08da15de060e62e65751e0dd60
Brian De Maio <bdemaio1@gmail.com>
2019-04-01 14:06:18 -0400

    Switched out routing for authetication into new modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
-import { SignupComponent } from "./auth/signup/signup.component";
-import { SigninComponent } from "./auth/signin/signin.component";
 import { AuthGuard } from "./auth/auth-guard.service";
 
 const appRoutes: Routes = [
@@ -11,9 +9,7 @@ const appRoutes: Routes = [
     path: "shopping-list",
     component: ShoppingListComponent,
     canActivate: [AuthGuard]
-  },
-  { path: "signup", component: SignupComponent },
-  { path: "signin", component: SigninComponent }
+  }
 ];
 
 @NgModule({
