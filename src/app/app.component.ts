8a3d8ec5ee0100986b7d8287f4b91f1acd5d9500
Brian De Maio <bdemaio1@gmail.com>
2019-04-01 09:25:51 -0400

    Adding firebase authentication setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import * as firebase from "firebase";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title: string = "angular-recipe";
   loadedFeature: string = "recipe";
+
+  ngOnInit() {
+    firebase.initializeApp({
+      apiKey: "AIzaSyCDx0hPQM3ABu_Pdr6Atc5mESQnD6ibKi8",
+      authDomain: "angular-recipe-9640e.firebaseapp.com"
+    });
+  }
 
   //Which navigation feature is requested will
   //determine the overall view displayed in the app component
---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,3 @@
+export class AuthService {
+  signUpUser(email: string, password: string) {}
+}
