989bd416fd22ebe094cd20f751b3504dd9f59d63
Brian De Maio <bdemaio1@gmail.com>
2019-04-01 15:24:45 -0400

    A little more cleanup into a core module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,34 +3,22 @@ import { NgModule } from "@angular/core";
 import { HttpClientModule } from "@angular/common/http";
 
 import { AppComponent } from "./app.component";
-import { HeaderComponent } from "./header/header.component";
-import { ShoppingListService } from "./shopping-list/shopping-list.service";
 import { AppRoutingModule } from "./app-routing.module";
-import { RecipeService } from "./recipes/recipe.service";
-import { DataStorageService } from "./shared/data-storage.service";
-import { AuthService } from "./auth/auth.service";
-import { AuthGuard } from "./auth/auth-guard.service";
 import { SharedModule } from "./shared/shared.module";
 import { ShoppingListModule } from "./shopping-list/shopping-list.module";
 import { AuthModule } from "./auth/auth.module";
-import { HomeComponent } from "./home/home.component";
+import { CoreModule } from "./core/core.module";
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, HomeComponent],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     ShoppingListModule,
     AppRoutingModule,
     HttpClientModule,
     SharedModule,
-    AuthModule
-  ],
-  providers: [
-    ShoppingListService,
-    RecipeService,
-    DataStorageService,
-    AuthService,
-    AuthGuard
+    AuthModule,
+    CoreModule
   ],
   bootstrap: [AppComponent]
 })
